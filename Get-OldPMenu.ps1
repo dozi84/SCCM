a45419907321eb97378338f813834ffe10cedaf4
dozi84 <102156417+dozi84@users.noreply.github.com>
2024-04-04 14:17:01 +0200

    Update Get-OldPMenu.ps1

--- Get-OldPMenu.ps1
+++ Get-OldPMenu.ps1
@@ -1,10 +1,11 @@
-$FileVersion = Get-Process | Where ProcessName -Like PMenu | Get-Process -FileVersionInfo
+$PMenuProcess = Get-Process PMenu* -IncludeUserName
+$NewestVersion = "2.2.2404.1"
 
-Foreach ($Process in $FileVersion) { 
-  If ($FileVersion.FileVersion -lt "2.2.2404.1") {
-      Write-Host Alte Version! -Foregroundcolor "Red"
+Foreach ($Process in $PMenuProcess) { 
+  If ($Process.FileVersion -lt $NewestVersion) {
+      Write-Host "Name:"$Process.Name "|" "Pfad:"$Process.Path "|" "Version:"$Process.FileVersion "|" "Benutzer:"$Process.UserName "|" "Alte Version!" -Foregroundcolor "Red"
   }
   Else {
-      Write-Host Aktuelle Version! -Foregroundcolor "Green"
+      Write-Host "Name:"$Process.Name "|" "Pfad:"$Process.Path "|" "Version:"$Process.FileVersion "|" "Benutzer:"$Process.UserName "|" "Aktuelle Version!" -Foregroundcolor "Green"
   }
 }
