b88b62d3069836a68cbd7b64272697689c7d3f5d
dozi84 <102156417+dozi84@users.noreply.github.com>
2024-04-04 15:26:13 +0200

    Update Get-OldPMenu.ps1

--- Get-OldPMenu.ps1
+++ Get-OldPMenu.ps1
@@ -1,11 +1,25 @@
+# Alle PMenu Prozesse auslesen
 $PMenuProcess = Get-Process PMenu* -IncludeUserName
+# Version des aktuellen PMenu
 $NewestVersion = "2.2.2404.1"
 
+Write-Host "-----------------------------------------"
+Write-Host ""
+Write-Host "Aktuelle PMenu-Version:" $NewestVersion -ForegroundColor Green
+Write-Host ""
+
+# Jeden Prozess ausgeben
 Foreach ($Process in $PMenuProcess) { 
   If ($Process.FileVersion -lt $NewestVersion) {
-      Write-Host "Name:"$Process.Name "|" "Pfad:"$Process.Path "|" "Version:"$Process.FileVersion "|" "Benutzer:"$Process.UserName "|" "Alte Version!" -Foregroundcolor "Red"
+      Write-Host "Start:"$Process.StartTime "|" "Name:"$Process.Name "|" "Pfad:"$Process.Path "|" "Version:"$Process.FileVersion "|" "Benutzer:"$Process.UserName -Foregroundcolor "Red"
   }
   Else {
-      Write-Host "Name:"$Process.Name "|" "Pfad:"$Process.Path "|" "Version:"$Process.FileVersion "|" "Benutzer:"$Process.UserName "|" "Aktuelle Version!" -Foregroundcolor "Green"
+      Write-Host "Start:"$Process.StartTime "|" "Name:"$Process.Name "|" "Pfad:"$Process.Path "|" "Version:"$Process.FileVersion "|" "Benutzer:"$Process.UserName -Foregroundcolor "Green"
   }
 }
+
+Write-Host ""
+Write-Host "-----------------------------------------"
+
+# [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+# iex (irm 'https://raw.githubusercontent.com/dozi84/SCCM/main/Get-OldPMenu.ps1')
