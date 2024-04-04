b0386c682a8849e4dc1dfe3b3a70882f6ac7c7ed
dozi84 <102156417+dozi84@users.noreply.github.com>
2024-04-04 08:20:10 +0200

    Update Get-OldPMenu.ps1

--- Get-OldPMenu.ps1
+++ Get-OldPMenu.ps1
@@ -1,10 +1,10 @@
 $FileVersion = Get-Process | Where ProcessName -Like PMenu | Get-Process -FileVersionInfo
 
 Foreach ($Process in $FileVersion) { 
-  If ($FileVersion.FileVersion -ge "2.2.2401.1") {
-      Write-Host Aktuell!
+  If ($FileVersion.FileVersion -lt "2.2.2404.1") {
+      Write-Host Alte Version! -Foregroundcolor "Red"
   }
   Else {
-      Write-Host Alt!
+      Write-Host Aktuelle Version! -Foregroundcolor "Green"
   }
 }
