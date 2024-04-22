f073466ed628d1f99ab033716f918f20a8e12438
dozi84 <102156417+dozi84@users.noreply.github.com>
2024-04-22 13:56:05 +0200

    Update WinRM.ps1

--- WinRM.ps1
+++ WinRM.ps1
@@ -1,3 +1,13 @@
-Get-PSSession -ComputerName PRIMUSS-TS-VW.rz.fh-ingolstadt.de -Credential espl_001\zieglerd-adm
+winrm get winrm/config
+
+Get-Item WSMan:\localhost\Client\TrustedHosts
+
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value 'DIAS-DEV-DZ'
+
+Get-PSSession -ComputerName PRIMUSS-TS-VW.rz.fh-ingolstadt.de -Credential espl_001\zieglerd-adm -UseSSL
 
 Enter-PSSession -ComputerName PRIMUSS-TS-VW.rz.fh-ingolstadt.de -Credential espl_001\zieglerd-adm -UseSSL
+
+Import-Module RemoteDesktop
+
+Get-RDCertificate | select role, Thumbprint
