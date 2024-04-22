d67c98a8d6f816cfc681f2904b944b1f258e1f39
dozi84 <102156417+dozi84@users.noreply.github.com>
2024-04-22 13:35:54 +0200

    Create WinRM.ps1

---
+++ WinRM.ps1
@@ -0,0 +1,3 @@
+Get-PSSession -ComputerName PRIMUSS-TS-VW.rz.fh-ingolstadt.de -Credential espl_001\zieglerd-adm
+
+Enter-PSSession -ComputerName PRIMUSS-TS-VW.rz.fh-ingolstadt.de -Credential espl_001\zieglerd-adm -UseSSL
