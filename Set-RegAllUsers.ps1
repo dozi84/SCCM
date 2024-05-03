c258dd46933a806d5116a575dfe9770da1da6273
dozi84 <102156417+dozi84@users.noreply.github.com>
2024-05-03 08:42:50 +0200

    Create Set-RegAllUsers.ps1

---
+++ Set-RegAllUsers.ps1
@@ -0,0 +1,41 @@
+# Regex pattern for SIDs
+$PatternSID = 'S-1-5-21-\d+-\d+\-\d+\-\d+$'
+ 
+# Get Username, SID, and location of ntuser.dat for all users
+$ProfileList = gp 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*' | Where-Object {$_.PSChildName -match $PatternSID} | 
+    Select  @{name="SID";expression={$_.PSChildName}}, 
+            @{name="UserHive";expression={"$($_.ProfileImagePath)\ntuser.dat"}}, 
+            @{name="Username";expression={$_.ProfileImagePath -replace '^(.*[\\\/])', ''}}
+ 
+# Get all user SIDs found in HKEY_USERS (ntuder.dat files that are loaded)
+$LoadedHives = gci Registry::HKEY_USERS | ? {$_.PSChildname -match $PatternSID} | Select @{name="SID";expression={$_.PSChildName}}
+ 
+# Get all users that are not currently logged
+$UnloadedHives = Compare-Object $ProfileList.SID $LoadedHives.SID | Select @{name="SID";expression={$_.InputObject}}, UserHive, Username
+ 
+# Loop through each profile on the machine
+Foreach ($item in $ProfileList) {
+    # Load User ntuser.dat if it's not already loaded
+    IF ($item.SID -in $UnloadedHives.SID) {
+        reg load HKU\$($Item.SID) $($Item.UserHive) | Out-Null
+    }
+ 
+    #####################################################################
+    # This is where you can read/modify a users portion of the registry 
+ 
+    # This example lists the Uninstall keys for each user registry hive
+    "{0}" -f $($item.Username) | Write-Output
+    Get-ItemProperty registry::HKEY_USERS\$($Item.SID)\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | 
+        Foreach {"{0} {1}" -f "   Program:", $($_.DisplayName) | Write-Output}
+    Get-ItemProperty registry::HKEY_USERS\$($Item.SID)\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | 
+        Foreach {"{0} {1}" -f "   Program:", $($_.DisplayName) | Write-Output}
+    
+    #####################################################################
+ 
+    # Unload ntuser.dat        
+    IF ($item.SID -in $UnloadedHives.SID) {
+        ### Garbage collection and closing of ntuser.dat ###
+        [gc]::Collect()
+        reg unload HKU\$($Item.SID) | Out-Null
+    }
+}
